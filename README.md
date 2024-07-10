da6620b0080e3d9ce614725bdafa38edca2e3db8
Miya784 <9burhanjehloh@gmail.com>
2024-07-10 16:06:03 +0700

    add Swagger

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ yarn run build
 yarn dev
 ```
 ###### Ts-node-express is a TypeScript-based project leveraging Express.js for robust backend development. Seamlessly integrating with PostgreSQL, it ensures secure database operations. The project includes comprehensive token management for user authentication with features like sign-in, sign-up, and token refresh capabilities. Perfect for scalable and secure web applications.
+
+####### - Express
+####### - PostgreSQL
+####### - JWT Token(accessToken & refreshToken)
+####### - Swagger
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express ,{ Request, Response } from 'express';
 import router from './routers';
 import { sync } from './models/index.model';
+import * as swaggerUi from "swagger-ui-express";
+import * as fs from "fs";
 
 const app = express();
 const port = 3000;
+
+// swagger docs
+if (process.env.NODE_ENV != "production") {
+  const swaggerFile: any = process.cwd() + "/src/swagger/swagger.json";
+  const swaggerData: any = fs.readFileSync(swaggerFile, "utf8");
+  const customCss: any = fs.readFileSync(process.cwd() + "/src/swagger/swagger.css", "utf8");
+  const swaggerDocument = JSON.parse(swaggerData);
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, undefined, undefined, customCss));
+}
 
 app.use('/',router);
 
